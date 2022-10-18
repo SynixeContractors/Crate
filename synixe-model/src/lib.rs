bf1ce5789cc905c1d5310dcc46912fcc99093cbf
Brett Mayson <brett@mayson.io>
2022-10-18 04:50:21 +0000

    create model

---
+++ synixe-model/src/lib.rs
@@ -0,0 +1,7 @@
+#![deny(missing_docs, missing_debug_implementations)]
+#![deny(clippy::pedantic)]
+#![warn(clippy::nursery, clippy::all)]
+
+//! Internal library for model definitions.
+
+pub mod missions;
---
+++ synixe-model/src/missions/mod.rs
@@ -0,0 +1,49 @@
+//! Running missions
+
+#![allow(clippy::use_self)] // serde false positive
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+/// A mission object
+pub struct Mission {
+    /// Mission id
+    pub id: String,
+    /// Mission name
+    pub name: String,
+    /// Mission summary
+    pub summary: String,
+    /// Mission description
+    pub description: String,
+    /// Mission type
+    pub typ: MissionType,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type), sqlx(rename_all = "lowercase"))]
+/// Mission type
+pub enum MissionType {
+    /// Standard Friday & Saturday mission
+    #[default]
+    Contract,
+    /// Non-standard mission
+    SubContract,
+    /// Training mission
+    Training,
+    /// Special mission
+    Special,
+    /// Other mission
+    Other,
+}
+
+impl From<u32> for MissionType {
+    fn from(value: u32) -> Self {
+        match value {
+            0 => Self::Contract,
+            1 => Self::SubContract,
+            2 => Self::Training,
+            3 => Self::Special,
+            _ => Self::Other,
+        }
+    }
+}
