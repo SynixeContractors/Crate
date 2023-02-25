ae261f71d9c821b6e5213ae4efd56adba772c835
BrettMayson <brett@mayson.io>
2023-02-24 18:11:57 -0600

    forgot files :|

---
+++ lib/events/src/events/reset.rs
@@ -0,0 +1,29 @@
+//! Events for the 2023 reset
+
+/// Interact with the database
+pub mod db {
+    use serenity::model::prelude::UserId;
+    use synixe_model::reset::UnclaimedKit;
+    use synixe_proc::events_requests;
+    use uuid::Uuid;
+
+    events_requests!(db.reset {
+        /// Get the unclaimed cert kits
+        struct UnclaimedKits {
+            /// The member to check
+            member: UserId,
+        } => (Result<Vec<UnclaimedKit>, String>)
+        /// Can claim a cert kit
+        struct CanClaim {
+            /// The member to check
+            member: UserId,
+        } => (Result<Option<Option<bool>>, String>)
+        /// Claime a cert kit
+        struct ClaimKit {
+            /// Member who claimed the kit
+            member: UserId,
+            /// Cert claimed
+            cert: Uuid,
+        } => (Result<(), String>)
+    });
+}
