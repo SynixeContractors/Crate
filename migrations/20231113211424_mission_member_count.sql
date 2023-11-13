ea70787c4b9519a304c8de93841b8763ee0bb57d
Brett Mayson <brett@mayson.io>
2023-11-13 22:35:47 +0000

    fix migration

--- migrations/20231113211424_mission_member_count.sql
+++ migrations/20231113211424_mission_member_count.sql
@@ -11,10 +11,11 @@ BEGIN
         INSERT INTO missions_member_count (member, count) VALUES (NEW.member, 1)
         ON CONFLICT (member) DO UPDATE SET count = missions_member_count.count + 1;
     END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER missions_member_count_increment
-BEFORE UPDATE ON gear_bank_deposits
+AFTER INSERT ON gear_bank_deposits
 FOR EACH ROW
 EXECUTE PROCEDURE missions_member_count_increment();
