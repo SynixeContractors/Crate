565810404432fde3f4a15d26046e442458c22da6
BrettMayson <brett@mayson.io>
2025-02-08 09:40:04 -0600

    update for rsync changes

--- bin/docker/src/handler/modpack.rs
+++ bin/docker/src/handler/modpack.rs
@@ -25,7 +25,7 @@ impl Handler for Request {
         let command = Command::new("rsync")
             .arg("-ur")
             .arg("--delete-after")
-            .arg("download@192.168.1.111:/home/download/mods/")
+            .arg("download@192.168.1.111:/")
             .arg(".")
             .current_dir("/arma/contracts/mods")
             .status()
