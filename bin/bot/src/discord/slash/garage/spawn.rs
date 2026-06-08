9173f0b5623a45c0564ea1143de0d85936818ca5
Brett Mayson <brett@mayson.io>
2026-06-08 18:35:23 +0000

    more explicit lock lifetime

--- bin/bot/src/discord/slash/garage/spawn.rs
+++ bin/bot/src/discord/slash/garage/spawn.rs
@@ -47,7 +47,7 @@ pub async fn spawn(
             .await;
     };
     interaction.reply("Waiting for lock").await?;
-    let _lock = SPAWN_LOCK.lock().await;
+    let lock = SPAWN_LOCK.lock().await;
     let Ok(Ok((db::Response::FetchVehicleInfo(Ok(info)), _))) = events_request_5!(
         bootstrap::NC::get().await,
         synixe_events::garage::db,
@@ -109,7 +109,7 @@ pub async fn spawn(
         interaction.reply("Failed to spawn vehicle").await?;
         return Ok(());
     };
-    match response {
+    let res = match response {
         SpawnResult::Yes => {
             std::mem::drop(interaction.reply("Vehicle spawning").await);
             if let Err(e) = events_request_5!(
@@ -173,5 +173,7 @@ pub async fn spawn(
                 .await
         }
         SpawnResult::NoPlayersNear => interaction.reply("Unable to spawn vehicle").await,
-    }
+    };
+    drop(lock);
+    res
 }
