f4a5eea7b3c3329d3350eb93839b7aafa3cb9f12
Brett Mayson <brett@mayson.io>
2026-08-31 17:12:35 -0600

    add animations to garage shop

---
+++ migrations/20260831224659_garage_shop_animations.sql
@@ -0,0 +1,21 @@
+-- Add migration script here
+ALTER TABLE garage_shop ADD COLUMN animations JSONB DEFAULT '[]'::JSONB;
+
+-- Add migration script here
+CREATE OR REPLACE FUNCTION garage_purchases_insert() RETURNS TRIGGER AS $$
+DECLARE purchasedBase UUID;
+DECLARE state JSONB;
+BEGIN
+    SELECT base INTO purchasedBase FROM garage_shop WHERE id = NEW.id;
+    IF purchasedBase IS NULL THEN
+        SELECT json_build_object(
+            'tex_source', (SELECT texture_source FROM garage_colors where id = NEW.id AND name = NEW.color LIMIT 1),
+            'phases', (SELECT animations FROM garage_shop where id = NEW.id LIMIT 1)
+        ) INTO state;
+        INSERT INTO garage_vehicles (plate, id, stored, state) VALUES (NEW.plate, NEW.id, TRUE, state);
+    ELSE
+        INSERT INTO garage_addons (id, count) VALUES (NEW.id, 1) ON CONFLICT (id) DO UPDATE SET count = garage_addons.count + 1;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
